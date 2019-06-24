f49373ba7faca405151b93d75b1e26ea1d3c94af
Aravind Reddy <39883677+n-aravind@users.noreply.github.com>
2019-06-23 23:12:24 -0700

    updated logic for cucumber reports

--- src/test/java/com/cucumber/rest/cucumberdemo/CucumberRunnerTest.java
+++ src/test/java/com/cucumber/rest/cucumberdemo/CucumberRunnerTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features",
-        glue = "com.cucumber.rest.cucumberdemo.steps")
+        glue = "com.cucumber.rest.cucumberdemo.steps",
+        plugin = {"pretty", "html:target/cucumber","json:target/cucumber/cucumber.json"})
 public class CucumberRunnerTest {
 }
